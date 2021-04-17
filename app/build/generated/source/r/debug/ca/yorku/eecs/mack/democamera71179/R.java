edba120ad6bc62cb9c7aaa3a2afd643177cbd1b8
Edward Shirinian <edshirinian@gmail.com>
2021-04-17 18:11:19 -0400

    Added search functionality where a user can input text in the search field and find pictures based on the input. If all the characters are deleted from the search field all of the pictures will repopulate

--- app/build/generated/source/r/debug/ca/yorku/eecs/mack/democamera71179/R.java
+++ app/build/generated/source/r/debug/ca/yorku/eecs/mack/democamera71179/R.java
@@ -14,9 +14,10 @@ public final class R {
     public static final int ic_gallery=0x7f010002;
     public static final int ic_leftarrow=0x7f010003;
     public static final int ic_rightarrow=0x7f010004;
-    public static final int ic_settings=0x7f010005;
-    public static final int ic_videocamera=0x7f010006;
-    public static final int icon=0x7f010007;
+    public static final int ic_search=0x7f010005;
+    public static final int ic_settings=0x7f010006;
+    public static final int ic_videocamera=0x7f010007;
+    public static final int icon=0x7f010008;
   }
   public static final class id {
     public static final int addTag=0x7f020000;
@@ -34,11 +35,12 @@ public final class R {
     public static final int imagecontainer=0x7f02000c;
     public static final int popupTitle=0x7f02000d;
     public static final int removeTagButton=0x7f02000e;
-    public static final int settings=0x7f02000f;
-    public static final int tag=0x7f020010;
-    public static final int tagView=0x7f020011;
-    public static final int textView=0x7f020012;
-    public static final int textview=0x7f020013;
+    public static final int search=0x7f02000f;
+    public static final int settings=0x7f020010;
+    public static final int tag=0x7f020011;
+    public static final int tagView=0x7f020012;
+    public static final int textView=0x7f020013;
+    public static final int textview=0x7f020014;
   }
   public static final class layout {
     public static final int imagegrid=0x7f030000;
@@ -48,6 +50,7 @@ public final class R {
   }
   public static final class menu {
     public static final int menu=0x7f040000;
+    public static final int options_menu=0x7f040001;
   }
   public static final class string {
     public static final int app_name=0x7f050000;
@@ -59,10 +62,15 @@ public final class R {
     public static final int imageCountString=0x7f050006;
     public static final int imageViewText=0x7f050007;
     public static final int imageviewtext=0x7f050008;
-    public static final int tagViewString=0x7f050009;
-    public static final int textString=0x7f05000a;
-    public static final int textViewString=0x7f05000b;
-    public static final int videoCountString=0x7f05000c;
-    public static final int videoviewtext=0x7f05000d;
+    public static final int search_hint=0x7f050009;
+    public static final int search_title=0x7f05000a;
+    public static final int tagViewString=0x7f05000b;
+    public static final int textString=0x7f05000c;
+    public static final int textViewString=0x7f05000d;
+    public static final int videoCountString=0x7f05000e;
+    public static final int videoviewtext=0x7f05000f;
+  }
+  public static final class xml {
+    public static final int searchable=0x7f060000;
   }
 }
